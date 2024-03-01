a0ba63f0cfcd48389b19b76e4b6ff6211fef2115
Rika Gorn <rikagx@gmail.com>
2024-03-01 15:34:30 -0500

    app troubleshooting

--- native_application/scripts/setup.sql
+++ native_application/scripts/setup.sql
@@ -1,62 +1,24 @@
--- Setup script for the workbench application.
-CREATE APPLICATION ROLE app_public;
-CREATE SCHEMA IF NOT EXISTS core;
-GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
+CREATE SCHEMA if not exists core;
+CREATE APPLICATION ROLE if not exists app_user;
 
-CREATE OR REPLACE WAREHOUSE tutorial_warehouse WITH
-  WAREHOUSE_SIZE='X-SMALL'
-  AUTO_SUSPEND = 180
-  AUTO_RESUME = true
-  INITIALLY_SUSPENDED=false;
+CREATE SCHEMA if not exists app_public;
+GRANT USAGE ON SCHEMA app_public TO APPLICATION ROLE app_user;
+CREATE OR REPLACE PROCEDURE app_public.start_app (pool_name varchar)
+   RETURNS string
+   LANGUAGE sql
+   as $$
+BEGIN
+CREATE SERVICE core.wb
+  IN COMPUTE POOL identifier(:pool_name)
+  spec=spec.yaml;
 
-CREATE COMPUTE POOL tutorial_compute_pool
-  MIN_NODES = 1
-  MAX_NODES = 1
-  INSTANCE_FAMILY = standard_1;
+/*CREATE OR REPLACE FUNCTION app_public.endpoint_url (TEXT VARCHAR)
+    RETURNS varchar
+    AS 'show endpoints in service core.wb' */
 
-CREATE DATABASE tutorial_db;
+RETURN 'Service successfully created';
+END;
+$$;
 
-CREATE SECURITY INTEGRATION snowservices_ingress_oauth
-  TYPE=oauth
-  OAUTH_CLIENT=snowservices_ingress
-  ENABLED=true;
-
-CREATE OR REPLACE NETWORK RULE ALLOW_ALL_RULE
-  TYPE = 'HOST_PORT'
-  MODE = 'EGRESS'
-  VALUES_LIST= ('0.0.0.0:443', '0.0.0.0:80');
-CREATE EXTERNAL ACCESS INTEGRATION ALLOW_ALL_EAI
-  ALLOWED_NETWORK_RULES = (ALLOW_ALL_RULE)
-  ENABLED = true;
-GRANT USAGE ON INTEGRATION ALLOW_ALL_EAI TO ROLE CONTAINER_USER_ROLE;
-
-CREATE IMAGE REPOSITORY CONTAINER_HOL_DB.PUBLIC.IMAGE_REPO;
-
-CREATE SERVICE wb
-   IN COMPUTE POOL tutorial_compute_pool
-   FROM SPECIFICATION $$
-spec:
-  container:
-  - name: rstudio
-    image: duloftf-posit-software-pbc.registry.snowflakecomputing.com/tutorial_db/data_schema/tutorial_repository/rstudio/rstudio-workbench:deploy
-    env:
-      RSW_LICENSE: ""
-    resources:
-      limits:
-        memory: 16Gi
-      requests:
-        memory: 4Gi
-  endpoint:
-  - name: e1
-    port: 8787
-    public: true
-  networkPolicyConfig:
-    allowInternetEgress: true
-   $$;
-
-GRANT USAGE ON SERVICE wb TO APPLICATION ROLE app_public;
-GRANT USAGE ON COMPUTE POOL tutorial_compute_pool TO APPLICATION ROLE app_public;
-GRANT MONITOR ON COMPUTE POOL tutorial_compute_pool TO APPLICATION ROLE app_public;
-GRANT ALL ON WAREHOUSE tutorial_warehouse TO ROLE test_role;
-GRANT OWNERSHIP ON DATABASE tutorial_db TO ROLE test_role;
-
+GRANT USAGE ON PROCEDURE app_public.start_app(varchar) TO APPLICATION ROLE app_user;
+/*GRANT USAGE ON PROCEDURE app_public.endpoint_url TO APPLICATION ROLE app_user; */
